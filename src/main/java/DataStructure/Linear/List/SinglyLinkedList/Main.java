f5b64f3d5a19e9a23f00364330a90e4c8997cab7
mostafa4099 <67890739+github-bits@users.noreply.github.com>
2022-12-05 18:20:43 +0600

    insert() method added

--- src/main/java/DataStructure/Linear/List/SinglyLinkedList/Main.java
+++ src/main/java/DataStructure/Linear/List/SinglyLinkedList/Main.java
@@ -1,38 +1,48 @@
 package DataStructure.Linear.List.SinglyLinkedList;
 
+import java.util.LinkedList;
+
 /**
  * @author Md. Golam Mostafa | mostafa.sna@gmail.com
  * @File DataStructure.Linear.List.SinglyLinkedList.Main.java: DataStructureAndAlgorithms
  * @CreationDate 11/29/2022 2:02 PM
  */
 public class Main {
-    public static void printSinglyLinkedList(SinglyLinkedList list){
-        Node currentNode = list.head;
-
-        while (null != currentNode){
-            System.out.println(currentNode.data);
-            currentNode = currentNode.next;
-        }
-
-    }
-
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
-        list.head = new Node(10); //head node
-
-        Node secondNode = new Node(12);
-        Node thirdNode = new Node(8);
-        Node fourthNode = new Node(6);
-        Node fifthNode = new Node(14);
+//        list.head = new Node(10); //head node
+//
+//        Node secondNode = new Node(12);
+//        Node thirdNode = new Node(8);
+//        Node fourthNode = new Node(6);
+//        Node fifthNode = new Node(14);
 
         //Making chain. Connect to previous node.
-        list.head.next = secondNode; //10-->12
-        secondNode.next = thirdNode; //10-->12-->8
-        thirdNode.next = fourthNode; //10-->12-->8-->6
-        fourthNode.next = fifthNode; //10-->12-->8-->6-->14
+//        list.head.next = secondNode; //10-->12
+//        secondNode.next = thirdNode; //10-->12-->8
+//        thirdNode.next = fourthNode; //10-->12-->8-->6
+//        fourthNode.next = fifthNode; //10-->12-->8-->6-->14
 //        fifthNode.next = null; //10-->12-->8-->6-->14-->null
 
         //print singly link list
-        printSinglyLinkedList(list);
+//        list.show();
+
+        //get length of singly link list
+//        System.out.println(list.length());
+
+        //insert beginning of the list
+//        list.insertFirst(7);
+//        list.show();
+
+        //insert end of the list
+//        list.insertLast(7);
+
+        list.insertFirst(8);
+        list.insertLast(4);
+        list.insertLast(5);
+        list.insert(6, 1);
+        list.removeFirst();
+        list.removeLast();
+        list.show();
     }
 }
--- src/main/java/DataStructure/Linear/List/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/DataStructure/Linear/List/SinglyLinkedList/SinglyLinkedList.java
@@ -1,5 +1,10 @@
 package DataStructure.Linear.List.SinglyLinkedList;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.security.InvalidParameterException;
+import java.util.EmptyStackException;
+
 /**
  * @author Md. Golam Mostafa | mostafa.sna@gmail.com
  * @File DataStructure.Linear.List.SinglyLinkedList.SinglyLinkedList.java: DataStructureAndAlgorithms
@@ -7,4 +12,147 @@ package DataStructure.Linear.List.SinglyLinkedList;
  */
 public class SinglyLinkedList {
     Node head;
+
+    /**
+     * display all element of list
+     */
+    public void show() {
+        Node currentNode = this.head;
+
+        while (null != currentNode) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+
+    /**
+     * display all element of list
+     */
+    public int length() {
+        int ctr = 0;
+        Node currentNode = this.head;
+
+        while (null != currentNode) {
+            ctr++;
+            currentNode = currentNode.next;
+        }
+        return ctr;
+    }
+
+    /**
+     * insert new item at the beginning of list
+     * create new node and assign previous head to the new node's next and assign new node in head of the list
+     *
+     * @param data
+     */
+    public void insertFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    /**
+     * insert new item at the end the of list
+     * @param data
+     */
+    public void insertLast(int data) {
+        Node newNode = new Node(data);
+
+        //if head node not null, traverse and find last node to make chain with new node.
+        //if head is null, add new node in head.
+        if (null != this.head) {
+            Node currentNode = this.head;
+
+            while (null != currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        } else {
+            this.head = newNode;
+        }
+    }
+
+    /**
+     * insert new item at index position of the list
+     * @param data
+     */
+    public void insert(int data, int index) {
+        if(index<0){
+            throw new InvalidParameterException("Index should be greater than 0");
+        }
+
+        if(index > this.length()){
+            throw new IndexOutOfBoundsException("Invalid List index.");
+        }
+
+        Node newNode = new Node(data);
+
+        if(index == 0){
+            // if provided index 0, insert data into head node and reindex previous head.
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            int ctr = 0;
+            Node previousNode = this.head;
+
+            //find out index-1 position node
+            while (ctr < index -1) {
+                previousNode = previousNode.next;
+                ctr++;
+            }
+
+            //get provided index position's node
+            Node currentNode = previousNode.next;
+
+            //place provided index position node after new node and make chain
+            newNode.next = currentNode;
+
+            //place new node in index position and make chain with previous node
+            previousNode.next = newNode;
+        }
+    }
+
+    /**
+     * remove first/head node of list and reassign head node
+     * @return deleted node
+     */
+    public Node removeFirst() {
+        if(null == this.head){
+            throw new RuntimeException("Empty list");
+        }
+        Node removedNode = head;
+        this.head = head.next;
+
+        return removedNode;
+    }
+
+    /**
+     * remove last node of list
+     * @return deleted node
+     */
+    public Node removeLast() {
+        Node currentNode = this.head;
+
+        if(null == currentNode){
+            throw new RuntimeException("Empty list");
+        }
+
+        //if list contain only one item
+        if(null == currentNode.next){
+            this.head = null;
+            return currentNode;
+        }
+
+        //find out previous node of last node and remove chain with it.
+        Node previousNode = null;
+        while (null != currentNode.next){
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        //break chain
+        previousNode.next = null;
+
+        return currentNode;
+    }
 }
